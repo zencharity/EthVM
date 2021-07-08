a942aa73ef1daf69dfb853d9d0703f5898858733
KANDA Daisuke <dai1975@gmail.com>
2021-07-08 18:39:44 +0900

    ci: remove zen comment type

--- newclient/commitlint.config.js
+++ newclient/commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
         'type-enum': [
             2,
             'always',
-            ['build', 'ci', 'devop', 'chore', 'docs', 'feat', 'fix', 'refactor', 'revert', 'style', 'lint', 'test', 'translations', 'zen']
+            ['build', 'ci', 'devop', 'chore', 'docs', 'feat', 'fix', 'refactor', 'revert', 'style', 'lint', 'test', 'translations']
         ]
     }
 }
