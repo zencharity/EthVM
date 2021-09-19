3dfd0644a17c1ca8e5c8289c957991a5565e7955
KANDA Daisuke <dai1975@gmail.com>
2021-09-19 21:47:17 +0900

    show info in build.sh

--- newclient/build.sh
+++ newclient/build.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-HTTP_LINK=https://ethvm-api.develop.zen-charity.net/
-WS_CLIENT=wss://ethvm-api.develop.zen-charity.net/graphql
+#HTTP_LINK=https://ethvm-api.develop.zen-charity.net/
+#WS_CLIENT=wss://ethvm-api.develop.zen-charity.net/graphql
 OPENSEA_API=
+
+if [ "x$HTTP_LINK" = "x" -o "x$WS_CLIENT" = "x" ]; then
+  echo "set HTTP_LINK and WS_CLIENT"
+  exit 1
+fi
+echo "HTTP_LINK=$HTTP_LINK"
+echo "WS_CLIENT=$WS_CLIENT"
 
 if [ -f src/configs.ts- ]; then
   cp src/configs.ts- src/configs.ts
@@ -12,6 +19,9 @@ fi
 sed -ie "s,process.env.VUE_APP_HTTP_LINK,'$HTTP_LINK',g" src/configs.ts || exit
 sed -ie "s,process.env.VUE_APP_WS_CLIENT,'$WS_CLIENT',g" src/configs.ts || exit
 sed -ie "s,process.env.VUE_APP_OPENSEA_API,'$OPENSEA_API',g" src/configs.ts || exit
+
+echo "config.ts is:"
+cat src/config.ts
 
 rm -rf build
 find src -name "*.tse" | xargs rm
